2e30f6ab8577eab5b6da6fd3eca6519714970517
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2023-01-27 00:28:53 +0100

    Changed Api Path for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_path: "http://lighter.netlify.app/.netlify/functions/",
+  api_path: "https://lighterteams.netlify.app/.netlify/functions/",
 };
